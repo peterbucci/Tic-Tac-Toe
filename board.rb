7bf4e825dfd3b72af6b7eff094cbdc3d669936b9
Peter <sb1285n@gmail.com>
2019-09-15 09:37:48 -0400

    Created Tile class. Added basic board functionality. The ability to set and get values on the grid.

--- board.rb
+++ board.rb
@@ -1,2 +1,23 @@
+require_relative "tile"
+
 class Board
+    def initialize
+        @grid = Array.new(3, Array.new(3) { Tile.new } )
+    end
+
+    def val(pos)
+        row, column = pos
+        @grid[row][column].value
+    end
+
+    def set_val(pos, new_val)
+        row, column = pos
+        @grid[row][column].value = new_val
+    end
 end
+
+board = Board.new
+p board.val([0,0])
+board.set_val([0,0], "X")
+p board.val([0,0])
+p board.val([1,1])
---
+++ tile.rb
@@ -0,0 +1,7 @@
+class Tile
+    attr_accessor :value
+
+    def initialize
+        @value = ""
+    end
+end
